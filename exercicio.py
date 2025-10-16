7091b6da657a1128fad29bb49174d0a03fdf60ec
NATHIELLE MOREIRA WALKER <24161>
2025-10-16 08:51:12 -0300

    primeiro commit

---
+++ exercicio.py
@@ -0,0 +1,30 @@
+class ContaBancaria:
+    def __init__(self, numero_conta, titular_conta):
+        self.numero_conta = numero_conta
+        self.titular_conta = titular_conta
+        self.saldo = 0.0
+    
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo =+ valor
+            print(f'Valor depositado de R${valor}, realizado com sucesso.')
+            
+        else:
+            print('Valor inválido')
+        
+    def sacar(self, valor):
+        if valor > 0:
+            if self.saldo >= valor:
+                self.saldo -= valor
+                print(f'Saque de R${valor} realizado com sucesso')
+            else: 
+                print("saldo insuficiente para realizar o saque")
+            
+conta = ContaBancaria(12345, "João da Silva")
+print(f"Saldo inicial da conta de {conta.titular_conta}: R${conta.saldo}")
+
+conta.depositar(1000)
+print(f"Saldo após depósito: R${conta.saldo}")
+
+conta.sacar(500)
+print(f"Saldo após saque: R${conta.saldo}")
